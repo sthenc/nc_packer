f3a993d44862094cb214b9470b273192a69dbec9
Stjepan Henc <sthenc@gmail.com>
2015-04-17 15:42:14 +0200

    Started script for computing mean and stddev values for speech signals

---
+++ compute_mean_stddev.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import numpy as np
+#import matplotlib as ml
+#import matplotlib.pyplot as plt
+
+import features
+import scipy.io.wavfile as wav
+
+# computes means and stddev for every mfcc bin for all the test files
+# in the entire development test set
+
+
