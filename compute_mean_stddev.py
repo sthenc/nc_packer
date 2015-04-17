76dca4f3dc59011b5fff5ac64c34c5c7d3b5303e
Stjepan Henc <sthenc@gmail.com>
2015-04-17 16:07:22 +0200

    mean&dev: Listing all files in the target directory

--- compute_mean_stddev.py
+++ compute_mean_stddev.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import numpy as np
 #import matplotlib as ml
@@ -10,4 +10,10 @@ import scipy.io.wavfile as wav
 # computes means and stddev for every mfcc bin for all the test files
 # in the entire development test set
 
+mypath = '/mnt/data/Fer/diplomski/nc_packer/test_data/PCCdata16kHz/devel/isolated/clean/'
 
+from os import listdir
+from os.path import isfile, join
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+
+print(onlyfiles)
