Computed some sort of mean and variance, doesn't look very correct...

compute_mean_stddev.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import numpy as np
 #import matplotlib as ml
@@ -16,4 +16,63 @@ from os import listdir
 from os.path import isfile, join
 onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
-print(onlyfiles)
+print('#files ' + str(len(onlyfiles)))
+
+from wav2mfcc import wav2mfcc
+
+NC = 39 # number of ceptral coefficients
+
+means = np.zeros(NC)
+stddevs = np.zeros(NC)
+
+N = 0
+
+for f in onlyfiles:
+	#f = onlyfiles[0]
+	mfccs = wav2mfcc(mypath + f)
+
+
+	#print(f)
+	sh = mfccs.shape
+	#print(sh)
+
+	lilN = sh[0]
+
+	for i in range(0, NC):
+		means[i] += sum(mfccs[0:lilN, i])
+
+
+	N = N + lilN
+
+print(N)
+print(means)
+
+means = means/N
+
+from pprint import pprint
+pprint(means)
+	
+
+for f in onlyfiles:
+#f = onlyfiles[0]
+
+	mfccs = wav2mfcc(mypath + f)
+
+	sh = mfccs.shape
+
+	lilN = sh[0]
+
+	for i in range(0, NC):
+		stddevs[i] += sum(mfccs[0:lilN, i] - means[i])**2
+
+#print(stddevs)
+
+stddevs /= N
+print(stddevs)
+
+from math import sqrt
+
+for i in range(0, NC):
+	stddevs[i] = sqrt(stddevs[i])
+	
+pprint(stddevs)

mfcc_visualize.py
@@ -10,6 +10,8 @@ import scipy.io.wavfile as wav
 from wav2mfcc import wav2mfcc
 
 
+
+
 #if __name__ == "__main__":
 
 datadir = '/mnt/data/Fer/diplomski/nc_packer/test_data/PCCdata16kHz/devel/isolated/'
@@ -27,9 +29,18 @@ fajl = 's1_bgaa9a.wav'
 
 # compute my mfcc features for this file
 
-mfcc_clean = wav2mfcc(datadir + cleandir + fajl)
+from mean_stddev import means, stddevs
 
-mfcc_noisy = wav2mfcc(datadir + lilnoise + fajl)
+#means[0] = 0
+#stddevs[0] = 1
+
+mfcc_clean_not_norm = wav2mfcc(datadir + cleandir + fajl)
+
+mfcc_clean = (mfcc_clean_not_norm - means) / stddevs
+
+mfcc_noisy_not_norm = wav2mfcc(datadir + lilnoise + fajl)
+
+mfcc_noisy = (mfcc_noisy_not_norm - means) / stddevs
 
 # mine .nc file
 