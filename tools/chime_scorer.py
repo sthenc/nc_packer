be13d58691b4d278e2e5b0df6ecf27dd8c1b68a6
Stjepan Henc <sthenc@gmail.com>
2015-06-14 19:38:00 +0200

    chime_scorer: simplified argument parsing

--- tools/chime_scorer.py
+++ tools/chime_scorer.py
@@ -6,35 +6,14 @@ import os
 
 import argparse
 parser = argparse.ArgumentParser()
+
+parser.add_argument('inset', choices=['test', 'val', 'train']) # all ? 
+parser.add_argument('recog', choices=['clean', 'reverb', 'noisy', 'retrain']) # all ? 
+
 parser.add_argument("testid", help="String to generate necessary folders etc.")   # can potentially delete data
 parser.add_argument("netname", help="Input autosave file")
-inset = parser.add_mutually_exclusive_group()
-inset.add_argument("-val", "--validate",
-	help="Score network on validation set",
-	action="store_true")
-inset.add_argument("-tst", "--test",
-	help="Score network on test set",
-	action="store_true")
-inset.add_argument("-trn", "--train",
-	help="Score network on training set",
-	action="store_true")
-recog = parser.add_mutually_exclusive_group()
-recog.add_argument("-cl", "--clean",
-	help="Score network output using the clean model",
-	action="store_true")
-recog.add_argument("-ns", "--noisy",
-	help="Score network output using the noisy model",
-	action="store_true")
-recog.add_argument("-rv", "--reverberated",
-	help="Score network output using the reverberated model",
-	action="store_true")
-recog.add_argument("-rt", "--retrain",
-	help="Score network output using a model retrained on training data",
-	action="store_true")
 
-
-# -all argument - compute all possible combinations for selected network
-# -del argument - delete temporary files (computed features)
+# -del argument - delete temporary files (computed features) ?
 
 args = parser.parse_args()
 
