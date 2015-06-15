6d7dab962a517d15ec001f11e476ea91d3a20469
Stjepan Henc <sthenc@gmail.com>
2015-06-15 13:11:40 +0200

    rename2mfcc: fixed bug with stripping suffixes

--- tools/rename2mfcc.sh
+++ tools/rename2mfcc.sh
@@ -8,7 +8,7 @@ for dir in `find . -maxdepth 1 -mindepth 1 -type d -printf '%f\n'` ; do
 		#echo $file
 		file_basename=`basename $file`
 		echo $file_basename
-		file_basename=${file_basename%.*}
+		file_basename=${file_basename%%.*}
 		
 		echo $file_basename
 		mv "$file" $dir/$file_basename".mfcc"
