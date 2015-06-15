3b3357f10401d0fb7d4583bc3f37c62cb6d9480c
Stjepan Henc <sthenc@gmail.com>
2015-06-15 13:06:04 +0200

    rename2mfcc: added stripping of all extensions

--- tools/rename2mfcc.sh
+++ tools/rename2mfcc.sh
@@ -1,12 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 cd "$1"
 
 for dir in `find . -maxdepth 1 -mindepth 1 -type d -printf '%f\n'` ; do
 	#echo $dir
-	for file in $dir/*.htk; do
+	for file in $dir/*.*; do
 		#echo $file
-		mv "$file" $dir/"`basename $file .htk`.mfcc"
+		file_basename=`basename $file`
+		echo $file_basename
+		file_basename=${file_basename%.*}
+		
+		echo $file_basename
+		mv "$file" $dir/$file_basename".mfcc"
+		
 		#echo "`basename $file .htk`.mfcc"
 	done
 done
